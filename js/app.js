0bee76de03d36215bc2ae564710775d86932001e
Diego Martin <diegomartinrecillas@outlook.com>
2019-01-21 13:36:10 +0100

    Main view html and styles; dinnerModel as an es6 class

--- js/app.js
+++ js/app.js
@@ -5,13 +5,12 @@ import DinnerModel from './model/dinnerModel.js';
 
 $(() => {
 	const model = new DinnerModel();
-
-	const view = new HomeView();
+	const container = $('#container');
+	const view = new HomeView(container, model);
 	/**
 	 * IMPORTANT: app.js is the only place where you are allowed to
 	 * use the $('someSelector') to search for elements in the whole HTML.
 	 * In other places you should limit the search only to the children
 	 * of the specific view you're working with (see exampleView.js).
 	 */
-
 });
